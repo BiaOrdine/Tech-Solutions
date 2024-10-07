dbd4f830af8c824526d04fe1a079f23e19fc6897
BiaOrdine <160529866+BiaOrdine@users.noreply.github.com>
2024-10-06 21:49:02 -0300

    Add files via upload

---
+++ ecommerce.js
@@ -0,0 +1,19 @@
+fetch('https://raw.githubusercontent.com/BiaOrdine/RAWs-techsolutions/refs/heads/main/produtos.json').then(resposta => resposta.json()).then(corpo =>{
+    console.log(corpo)
+
+    document.getElementById('imagem1').innerHTML = corpo.imagem1;
+    document.getElementById('nome1').innerHTML = corpo.nome1;
+    document.getElementById('autor1').innerHTML = corpo.autor1;
+    document.getElementById('preco1').innerHTML = corpo.preco1;
+
+    document.getElementById('imagem2').innerHTML = corpo.imagem2;
+    document.getElementById('nome2').innerHTML = corpo.nome2;
+    document.getElementById('autor2').innerHTML = corpo.autor2;
+    document.getElementById('preco2').innerHTML = corpo.preco2;
+
+    document.getElementById('imagem3').innerHTML = corpo.imagem3;
+    document.getElementById('nome3').innerHTML = corpo.nome3;
+    document.getElementById('autor3').innerHTML = corpo.autor3;
+    document.getElementById('preco3').innerHTML = corpo.preco3;
+
+})
---
+++ tema.js
@@ -0,0 +1,72 @@
+function artista1(){
+    fetch('https://raw.githubusercontent.com/BiaOrdine/RAWs-techsolutions/refs/heads/main/tema.json').then(resposta => resposta.json()).then(artistas =>{
+    
+     document.getElementById('imagem').innerHTML = artistas.foto1;
+     document.getElementById('nomeartista').innerHTML = artistas.artista1;
+     document.getElementById('dataartista').innerHTML = artistas.data1;
+     document.getElementById('texto').innerHTML = artistas.texto1;
+
+     })
+}
+
+function artista2(){
+    fetch('https://raw.githubusercontent.com/BiaOrdine/RAWs-techsolutions/refs/heads/main/tema.json').then(resposta => resposta.json()).then(artistas =>{
+    
+     document.getElementById('imagem').innerHTML = artistas.foto2;
+     document.getElementById('nomeartista').innerHTML = artistas.artista2;
+     document.getElementById('dataartista').innerHTML = artistas.data2;
+     document.getElementById('texto').innerHTML = artistas.texto2;
+
+     })
+}
+
+function artista3(){
+    fetch('https://raw.githubusercontent.com/BiaOrdine/RAWs-techsolutions/refs/heads/main/tema.json').then(resposta => resposta.json()).then(artistas =>{
+    
+     document.getElementById('imagem').innerHTML = artistas.foto3;
+     document.getElementById('nomeartista').innerHTML = artistas.artista3;
+     document.getElementById('dataartista').innerHTML = artistas.data3;
+     document.getElementById('texto').innerHTML = artistas.texto3;
+
+     })
+}
+
+function artista4(){
+    fetch('https://raw.githubusercontent.com/BiaOrdine/RAWs-techsolutions/refs/heads/main/tema.json').then(resposta => resposta.json()).then(artistas =>{
+    
+     document.getElementById('imagem').innerHTML = artistas.foto4;
+     document.getElementById('nomeartista').innerHTML = artistas.artista4;
+     document.getElementById('dataartista').innerHTML = artistas.data4;
+     document.getElementById('texto').innerHTML = artistas.texto4;
+
+     })
+}
+
+function artista5(){
+    fetch('https://raw.githubusercontent.com/BiaOrdine/RAWs-techsolutions/refs/heads/main/tema.json').then(resposta => resposta.json()).then(artistas =>{
+    
+     document.getElementById('imagem').innerHTML = artistas.foto5;
+     document.getElementById('nomeartista').innerHTML = artistas.artista5;
+     document.getElementById('dataartista').innerHTML = artistas.data5;
+     document.getElementById('texto').innerHTML = artistas.texto5;
+
+     })
+}
+
+function artista6(){
+    fetch('https://raw.githubusercontent.com/BiaOrdine/RAWs-techsolutions/refs/heads/main/tema.json').then(resposta => resposta.json()).then(artistas =>{
+    
+     document.getElementById('imagem').innerHTML = artistas.foto6;
+     document.getElementById('nomeartista').innerHTML = artistas.artista6;
+     document.getElementById('dataartista').innerHTML = artistas.data6;
+     document.getElementById('texto').innerHTML = artistas.texto6;
+
+     })
+}
+ 
+
+ 
+ 
+ 
+ 
+ 
